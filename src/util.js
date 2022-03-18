4af50bd1330136f78811a3635734bfcf2114f5dc
David Adewole <adeoluwadavid@gmail.com>
2022-03-18 12:41:08 +0100

    toptal

---
+++ src/util.js
@@ -0,0 +1,14 @@
+export function performLogin({ email, password }) {
+  console.log(email)
+  console.log(password)
+  const delay = (0.5 + Math.random() * 2) * 1000;
+  return new Promise((resolve, reject) => {
+    setTimeout(function () {
+      if (password === "correct" && !!email) {
+        resolve();
+      } else {
+        reject(new Error("Invalid credentials"));
+      }
+    }, delay);
+  });
+}
